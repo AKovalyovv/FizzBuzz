216dae959faac153762c4007c403e8c7956b8661
AKovalyovv <aleksandr.kovalyovv@gmail.com>
2023-09-11 16:31:55 -0400

    finished Reduce module

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int step = 0;
+
+        while (i > 0) {
+            if (i % 2 == 0) {
+                i /= 2;
+            }
+            else
+                i -= 1;
+            step += 1;
+        }
+
+        System.out.println(step);
+    }
 }
