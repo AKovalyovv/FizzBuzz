8bacb67d492a290804d16c7a05ae5080b08fe745
AKovalyovv <aleksandr.kovalyovv@gmail.com>
2023-09-11 16:26:56 -0400

    Multiples class implemented

---
+++ src/Multiples.java
@@ -0,0 +1,9 @@
+public class Multiples {
+    public static void main(String[] args) {
+        int num3 = 999 / 3;
+        int num5 = 999 / 5;
+        int num15 = 999 / 15;
+
+        System.out.println((num3 + num5 - num15));
+    }
+}
